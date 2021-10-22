87366aac5f191b28bed3854c38250d88664f83d2
zxc85166 <jimmya011@gmail.com>
2021-10-23 00:02:44 +0800

    path

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@ import Navbar from "./components/Navbar.vue";
 import Footer from "./components/Footer.vue";
 const links = [
   { id: 1, name: "關於人晨", path: "/about" },
-  { id: 2, name: "教育課程", path: "/Foo" },
-  { id: 3, name: "商品資訊", path: "/Foo" },
-  { id: 4, name: "場地租借", path: "/Foo" },
-  { id: 5, name: "合作店家", path: "/Foo" },
-  { id: 6, name: "聯絡我們", path: "/Foo" },
+  { id: 2, name: "教育課程", path: "/education" },
+  { id: 3, name: "商品資訊", path: "/information" },
+  { id: 4, name: "場地租借", path: "/rent" },
+  { id: 5, name: "合作店家", path: "/Cooperative" },
+  { id: 6, name: "聯絡我們", path: "/contact" },
 ];
 </script>
 
